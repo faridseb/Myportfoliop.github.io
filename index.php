758c8351c1e1259694351ac2f786ead2883b7098
Farid SEBOU <seboufarid43@gmail.com>
2025-07-15 20:24:58 +0000

    first

---
+++ index.php
@@ -0,0 +1,382 @@
+<!DOCTYPE HTML>
+<!--
+	Landed by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>MyPortfolio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload landing" style="font-weight: 450;">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					<h1 id="logo" style="font-size: 40px;"><a href="index.html">Portfolio</a></h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="index.html">Acceuil</a></li>
+							<li>
+								<a href="#four">Competences</a>
+							</li>
+							<li>
+								<a href="#">A propos</a>
+							</li>
+							<li><a href="#fife">Realisations</a></li>
+							<li><a href="#five" class="button primary">Contact</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+					<div class="content">
+						<header>
+							<h2>Je suis SEBOU Farid</h2>
+							<p>Developpeur web.<br />
+						    Developpeur front-end et back-end.</p>
+							<a href="#five" class="button primary " style="margin: 40px 20px;">Profil</a>
+							<a href="assets/mycv.pdf" class="button primary" download="MonCV">Mon CV</a>
+							<ul class="icons">
+								<li><a href="https://x.com/sebou_fari16891?t=BP7bHz1rhQgHE0iIMJtHjA&s=09" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a href="https://www.facebook.com/profile.php?id=100070130408601" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.linkedin.com/in/farid-sebou-679306354" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+								<li><a href=" https://www.instagram.com/rid_stack_?igsh=YzljYTk1ODg3Zg==" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+								<li><a href="https://github.com/faridseb" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+								
+							</ul>
+						</header>
+						
+						<span class="image"><img src="images/photof.JPG" alt="" /></span>
+					</div>
+					
+					<!-- <a href="#one" class="goto-next scrolly">Next</a> -->
+				</section>
+				<section id="four" class="wrapper style1 special fade-up">
+					<div class="container">
+						<header class="major">
+							<h2>Mes Competences</h2>
+							<p>Voici toutes mes competences que j'ai pu acquerir au cours de mon experience</p>
+						</header>
+						<div class="box alt">
+							<div class="row gtr-uniform">
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/html5.png" style="width: 24%;" alt=""></span>
+									<h3>HTML5</h3>
+									<progress value="85" max="100"></progress>
+									<p style="font-weight: 800;">85%</p>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/css3.png" style="width: 20%;" alt=""></span>
+									<h3>CSS3</h3>
+									<progress value="80" max="100"></progress>
+									<p style="font-weight: 800;">80%</p>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/js5.png" style="width: 20%;" alt=""></span>
+									<h3>Javascript</h3>
+									<progress value="50" max="100"></progress>
+									<p style="font-weight: 800;">50%</p>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/php.png" style="width: 20%;" alt=""></span>
+									<h3>Php</h3>
+									<progress value="75" max="100"></progress>
+									<p style="font-weight: 800;">75%</p>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/sql.png" style="width: 20%;" alt=""></span>
+									<h3>SQL</h3>
+									<progress value="70" max="100"></progress>
+									<p style="font-weight: 800;">70%</p>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/laravel.png" style="width: 20% ;" alt=""></span>
+									<h3>Laravel</h3>
+									<progress value="20" max="100"></progress>
+									<p style="font-weight: 800;">20%</p>
+									
+								</section>
+									<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/delphi.png" style="width: 20%;" alt=""></span>
+									<h3>Delphi Borland</h3>
+									<progress value="80" max="100"></progress>
+									<p style="font-weight: 800;">80%</p>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/py.png" style="width: 18% ;" alt=""></span>
+									<h3>Python</h3>
+									<progress value="40" max="100"></progress>
+									<p style="font-weight: 800;">40%</p>
+									
+								</section>
+								<!-- <section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/sql.jpg" style="width:60%;" alt=""></span>
+									<h3>BANQUIS</h3>
+									<p>Application Desktop realiser pour la gestion d'une banque</p>
+									<a href="#five" class="button primary " style="margin: 40px 20px;">Voir</a>
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/event.png" style="width:63%;" alt=""></span>
+									<h3>ENVENTPRO</h3>
+									<p>Application Desktop qui fais la gestion des evenements</p>
+									<a href="#five" class="button primary " style="margin: 40px 20px;">Voir</a>
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/kadodo.png" style="width: 100%;" alt=""></span>
+									<h3>KADODO</h3>
+									<p>Intranet qui permet la gestion d'une ecole faites en php,bootstrap et Javascript </p>
+									<a href="#five" class="button primary " style="margin: 40px 20px;">Voir</a>
+								</section> -->
+							</div>
+						</div>
+						
+					</div>
+				</section>
+				
+				<section id="fix" class="wrapper style1 special fade-up">
+					<header class="major">
+							<h2>Profil & Experience</h2>
+							<p>Voici toutes mes competences que j'ai pu acquerir au cours de mon experience</p>
+					</header>
+					<div class="box alt">
+							<div class="row gtr-uniform">
+								<section class="col-6 col-6-medium col-12-xsmall">
+									<h3>Profil</h3>
+									<p>Je suis développeur web  qui apprend et met en pratique de nouvelles compétences, curieux de comprendre comment les choses fonctionnent en coulisses, notamment sur le web.</p>
+									
+								</section>
+								<section class="col-6 col-6-medium col-12-xsmall">
+									<h3>Experience</h3>
+									<ul>
+										<li><p style="font-weight: 700;">Conception d'une application de gestion d'un intranet (Avril 2025)</p></li>
+										<p>Conception des interfaces front-end et de l'architecture back-end</p>
+									</ul>
+
+										<ul>
+										<li><p style="font-weight: 700;">Conception d'une application de Ecommerce (Aout 2024)</p></li>
+										<p>Conception en integralite du site de Ecommere du back-end au front-end</p>
+										</ul>
+
+										<ul>
+										<li><p style="font-weight: 700;">Conception d'applications pour des particuliers (Mai 2025)</p></li>
+										<p>Conception d'applications Desktop en Delphi Bordland</p>
+										</ul>
+									
+									
+									
+								</section>
+							</div>
+						</div>
+				</section>
+			<!-- One 
+				<section id="one" class="spotlight style1 bottom">
+					<span class="image fit main"><img src="images/image" alt="" /></span>
+					<div class="content">
+						<div class="container">
+							<header class="major">
+								<h2>Mes Competences</h2>
+								<p>Voici toutes les realisations que j'ai eu a realiser au cours de mon parcour de Developpeur</p>
+							</header>
+							
+							<div class="row">
+								
+								<div class="col-4 col-12-medium">
+									<header>
+										<h2>Mon Profil</h2>
+										<p>Je suis un Developpeur Junior</p>
+									</header>
+								</div>
+								<div class="col-4 col-12-medium">
+									<p>Je fais du front-end j'ai deja eu a faire des experience avec des langages tel que html-css,bootstrap</p>
+								</div>
+								<div class="col-4 col-12-medium">
+									<p>Morbi enim nascetur et placerat lorem sed iaculis neque ante
+									adipiscing adipiscing metus massa. Blandit orci porttitor semper.
+									Arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer
+									mi sed nascetur cep aliquet augue varius tempus. Feugiat lorem
+									ipsum dolor nullam.</p>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<a href="#two" class="goto-next scrolly">Next</a>
+				</section>-->
+
+			<!-- Two -->
+				<!-- <section id="two" class="spotlight style2 right">
+					<span class="image fit main"><img src="images/pic03.jpg" alt="" /></span>
+					<div class="content">
+						<header>
+							<h2>Interdum amet non magna accumsan</h2>
+							<p>Nunc commodo accumsan eget id nisi eu col volutpat magna</p>
+						</header>
+						<p>Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor lorem et accumsan consequat adipiscing lorem.</p>
+						<ul class="actions">
+							<li><a href="#" class="button">Learn More</a></li>
+						</ul>
+					</div>
+					<a href="#three" class="goto-next scrolly">Next</a>
+				</section> -->
+
+			<!-- Three -->
+				<!-- <section id="three" class="spotlight style3 left">
+					<span class="image fit main bottom"><img src="images/pic04.jpg" alt="" /></span>
+					<div class="content">
+						<header>
+							<h2>Interdum felis blandit praesent sed augue</h2>
+							<p>Accumsan integer ultricies aliquam vel massa sapien phasellus</p>
+						</header>
+						<p>Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor lorem et accumsan consequat adipiscing lorem.</p>
+						<ul class="actions">
+							<li><a href="#" class="button">Learn More</a></li>
+						</ul>
+					</div>
+					<a href="#four" class="goto-next scrolly">Next</a>
+				</section> -->
+
+			<!-- Four -->
+				<section id="fife" class="wrapper style1 special fade-up">
+					<div class="container">
+						<header class="major">
+							<h2>Mes realisations</h2>
+							<p>Voici toutes les realisations que j'ai eu a realiser au cours de mon parcour de Developpeur</p>
+						</header>
+						<div class="box alt">
+							<div class="row gtr-uniform">
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/ama.png" style="width: 100%;" alt=""></span>
+									<h3>Amazon Shop</h3>
+									<p>Un site Ecommerce fais en php et en bootstrap</p>
+									<a href="#five" class="button primary " style="margin: 40px 20px;">Voir</a>
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/sante.png" style="width: 100%;" alt=""></span>
+									<h3>CONSANTE</h3>
+									<p>Application de gestion des rendez vous d'une clinique fais en php egalement</p>
+									<a href="#five" class="button primary " style="margin: 40px 20px;">Voir</a>
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/projet.png" style="width: 100%;" alt=""></span>
+									<h3>TACHTEC</h3>
+									<p>Un site qui permet la gestion des projets informatiques</p>
+									<a href="#five" class="button primary " style="margin: 40px 20px;">Voir</a>
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/banquw.png" style="width:60%;" alt=""></span>
+									<h3>BANQUIS</h3>
+									<p>Application Desktop realiser pour la gestion d'une banque</p>
+									<a href="#five" class="button primary " style="margin: 40px 20px;">Voir</a>
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/event.png" style="width:63%;" alt=""></span>
+									<h3>ENVENTPRO</h3>
+									<p>Application Desktop qui fais la gestion des evenements</p>
+									<a href="#five" class="button primary " style="margin: 40px 20px;">Voir</a>
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+									<span><img src="images/kadodo.png" style="width: 100%;" alt=""></span>
+									<h3>KADODO</h3>
+									<p>Intranet qui permet la gestion d'une ecole faites en php,bootstrap et Javascript </p>
+									<a href="#five" class="button primary " style="margin: 40px 20px;">Voir</a>
+								</section>
+							</div>
+						</div>
+						
+					</div>
+				</section>
+
+			<!-- Five -->
+				<section id="five" class="wrapper style2 special fade">
+					<div class="container">
+						<header>
+							<h2>Me contacter</h2>
+							<!-- <p>Ante metus praesent faucibus ante integer id accumsan eleifend</p> -->
+						</header>
+						<form method="POST"  id="contactForm" action="traitement.php" class="cta">
+							<div class="row gtr-uniform gtr-50">
+								<div class="col-12 col-12-xsmall"><input type="email" name="email_expediteur" id="email" placeholder="Votre adresse email" required/></div>
+								<div class="col-12 col-12-xsmall"><textarea name="message_texte" id="" placeholder="Entrer votre texte" required></textarea></div>
+								<div class="col-12 col-12-xsmall"><input type="submit" value="Commencer" class="fit primary" /></div>
+								
+							</div>
+						</form>
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+								<li><a href="https://x.com/sebou_fari16891?t=BP7bHz1rhQgHE0iIMJtHjA&s=09" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a href="https://www.facebook.com/profile.php?id=100070130408601" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.linkedin.com/in/farid-sebou-679306354" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+								<li><a href=" https://www.instagram.com/rid_stack_?igsh=YzljYTk1ODg3Zg==" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+								<li><a href="https://github.com/faridseb" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="#header">By RidStack</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script>
+        const contactForm = document.getElementById('contactForm');
+
+        // Gérer la soumission du formulaire
+        contactForm.addEventListener('submit', function(event) {
+            event.preventDefault(); // Empêche l'envoi traditionnel du formulaire (qui rechargerait la page)
+
+            const formData = new FormData(this); // Récupère toutes les données du formulaire
+
+            fetch(this.action, {
+                method: this.method,
+                body: formData
+            })
+            .then(response => {
+                // Vérifie si la réponse est OK (statut HTTP 200)
+                if (response.ok) {
+                    return response.text(); // Lit la réponse du serveur comme texte
+                }
+                throw new Error('Erreur réseau ou réponse serveur incorrecte.');
+            })
+            .then(data => {
+                // Ici, 'data' contient la réponse de votre script PHP
+                // Si PHP renvoie "succès", on affiche l'alerte
+                if (data.includes("succès")) { // Assurez-vous que votre PHP renvoie "succès" en cas de réussite
+                    alert("Votre message a été envoyé avec succès !"); // Affiche l'alerte
+                    contactForm.reset(); // Réinitialise le formulaire après succès
+                } else {
+                    alert("Une erreur est survenue lors de l'envoi : " + data);
+                }
+            })
+            .catch(error => {
+                console.error('Erreur lors de la soumission du formulaire:', error);
+                alert("Une erreur inattendue est survenue. Veuillez réessayer.");
+            });
+        });
+    </script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
